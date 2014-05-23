ca13d6c232b99cbe5d53f7293b3e90451489b482
felipeleao <felipe@fleao.com.br>
2014-05-23 01:57:40 -0300

    Addition of publications
    - Added the citations and titles for publications
    - Added all the info for Tech reports

--- publications/index.php
+++ publications/index.php
@@ -9,7 +9,7 @@
   <div class="full-page-content">
 
     <h1>Publications</h1>
-    <p>Below are described informations about my publications, comprising both academic papers and Tech Reports.</p>
+    <p>Below are described informations about my publications, comprising both Academic  Papers and Tech Reports.</p>
 
     <div id="push"></div>
 
@@ -36,9 +36,9 @@
           </div>
         </div>
         <div class="row">
-          <div class="col-md-2"><p>Reference</p></div>
+          <div class="col-md-2"><p>Citation</p></div>
           <div class="col-md-10">
-            <p>LEÃO, F., REVOREDO, K., BAIÃO, F. “Learning Well-Founded Ontologies through Word Sense Disambiguation”. Brazilian Conference on Intelligent Systems, Fortaleza - CE, 2013. (<a href="#">bibitex</a>)</p>
+            <p class="small">LEÃO, F., REVOREDO, K., BAIÃO, F. “Learning Well-Founded Ontologies through Word Sense Disambiguation”. Brazilian Conference on Intelligent Systems, Fortaleza - CE, 2013. </p>
           </div>
         </div>
         <div class="row">
@@ -76,9 +76,9 @@
           </div>
         </div>
         <div class="row">
-          <div class="col-md-2"><p>Reference</p></div>
+          <div class="col-md-2"><p>Citation</p></div>
           <div class="col-md-10">
-            <p>LEÃO, F., REVOREDO, K., BAIÃO, F. “Desambiguação de Sentidos para o Aprendizado Automático de Modelos Conceituais bem Fundamentados em OntoUML”. Workshop de Teses e Dissertações em Sistemas de Informação, Anais do 9º Simpósio Brasileiro de Sistemas de Informação, João Pessoa - PB, 2013 (<a href="#">bibitex</a>)</p>
+            <p class="small">LEÃO, F., REVOREDO, K., BAIÃO, F. “Desambiguação de Sentidos para o Aprendizado Automático de Modelos Conceituais bem Fundamentados em OntoUML”. Workshop de Teses e Dissertações em Sistemas de Informação, Anais do 9º Simpósio Brasileiro de Sistemas de Informação, João Pessoa - PB, 2013 </p>
           </div>
         </div>
         <div class="row">
@@ -114,9 +114,9 @@
           </div>
         </div>
         <div class="row">
-          <div class="col-md-2"><p>Reference</p></div>
+          <div class="col-md-2"><p>Citation</p></div>
           <div class="col-md-10">
-            <p>LEÃO, F., DIIRR, T., BAIAO, F., REVOREDO, K. “Construção de Modelos Conceituais a Partir de Textos com Apoio de Tipos Semânticos”. Anais do 5º ONTOBRAS – Seminário de Pesquisas em Ontologias no Brasil, Recife, PE, 2012 (<a href="#">bibitex</a>)</p>
+            <p class="small">LEÃO, F., DIIRR, T., BAIAO, F., REVOREDO, K. “Construção de Modelos Conceituais a Partir de Textos com Apoio de Tipos Semânticos”. Anais do 5º ONTOBRAS – Seminário de Pesquisas em Ontologias no Brasil, Recife, PE, 2012 </p>
           </div>
         </div>
         <div class="row">
@@ -151,9 +151,9 @@
           </div>
         </div>
         <div class="row">
-          <div class="col-md-2"><p>Reference</p></div>
+          <div class="col-md-2"><p>Citation</p></div>
           <div class="col-md-10">
-            <p>LEÃO, F. ; AZEVEDO, L. ; BAIAO, F. ; CAPELLI, C. “Enforcing Authorization Rules in Information Systems Using Identity Propagation and Role-Based Access Control Mechanisms”. In: IADIS Applied Computing 2011, Rio de Janeiro, RJ, 2011. Proceedings of IADIS International Conference IADIS Applied Computing 2011 (<a href="#">bibitex</a>)</p>
+            <p class="small">LEÃO, F. ; AZEVEDO, L. ; BAIAO, F. ; CAPELLI, C. “Enforcing Authorization Rules in Information Systems Using Identity Propagation and Role-Based Access Control Mechanisms”. In: IADIS Applied Computing 2011, Rio de Janeiro, RJ, 2011. Proceedings of IADIS International Conference IADIS Applied Computing 2011 </p>
           </div>
         </div>
         <div class="row">
@@ -188,9 +188,9 @@
           </div>
         </div>
         <div class="row">
-          <div class="col-md-2"><p>Reference</p></div>
+          <div class="col-md-2"><p>Citation</p></div>
           <div class="col-md-10">
-            <p>LEÃO, F. ; REVOREDO, K. ; BAIAO, F. “Um Framework para Refinamento de Ontologias Através de Revisão de Teorias”. In: XXI Congresso da Sociedade Brasileira de Computação, 2011, Natal, RN. Anais do XXXI Congresso da Sociedade Brasileira de Computação, páginas: 821-832 (<a href="#">bibitex</a>)</p>
+            <p class="small">LEÃO, F. ; REVOREDO, K. ; BAIAO, F. “Um Framework para Refinamento de Ontologias Através de Revisão de Teorias”. In: XXI Congresso da Sociedade Brasileira de Computação, 2011, Natal, RN. Anais do XXXI Congresso da Sociedade Brasileira de Computação, páginas: 821-832 </p>
           </div>
         </div>
         <div class="row">
@@ -225,9 +225,9 @@
           </div>
         </div>
         <div class="row">
-          <div class="col-md-2"><p>Reference</p></div>
+          <div class="col-md-2"><p>Citation</p></div>
           <div class="col-md-10">
-            <p>•	LEÃO, F. “ORION: Um Framework para Revisão de Ontologias através de Técnicas de Revisão de Teorias”. Monografia de Conclusão de Curso para obtenção do título de Bacharel em Sistemas de Informação. Universidade Federal do Estado do Rio de Janeiro (UNIRIO), Rio de Janeiro, RJ, 2011. (<a href="#">bibitex</a>)</p>
+            <p class="small">LEÃO, F. “ORION: Um Framework para Revisão de Ontologias através de Técnicas de Revisão de Teorias”. Monografia de Conclusão de Curso para obtenção do título de Bacharel em Sistemas de Informação. Universidade Federal do Estado do Rio de Janeiro (UNIRIO), Rio de Janeiro, RJ, 2011. </p>
           </div>
         </div>
         <div class="row">
@@ -245,7 +245,44 @@
           <div class="col-md-12">
             <h3>
               <span class="glyphicon glyphicon-chevron-right"></span>
-              ORION–Um Framework para Refinamento de Ontologias Através de Técnicas de Revisão de Teorias
+              Controle de Acesso a Dados em Sistemas de Informação através de Mecanismos de Propagação de Identidade e Execução de Regras de Autorização
+            </h3>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 authors"><p>Felipe Leão, Sérgion Puntar, Leonardo Azevedo, Fernanda Baião and Claudia Capelli</p></div>
+        </div>
+        <div class="row">
+          <div class="col-md-2"><p>Abstract</p></div>
+          <div class="col-md-10">
+            Ontology have been frequently used for knowledge representation. As it evolves, however,
+            keeping its consistency becomes a costly and complex task, if no support is provided. This work proposes ORION
+            – Ontology Refinement through theory revisION, which applies existing Theory Revision techniques to support
+            Ontology evolution. Preliminary tests were successful in obtaining a more accurate refined Ontology.
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-2"><p>Citation</p></div>
+          <div class="col-md-10">
+            <p class="small">LEÃO, F. ; PUNTAR, S. ; Azevedo, L. ; BAIAO, F. ; CAPELLI, C. . “Controle de Acesso a Dados em Sistemas de Informação através de Mecanismos de Propagação de Identidade e Execução de Regras de Autorização”. In: SBSI - Simpósio Brasileiro de Sistemas de Informação, 2011, Salvador, BA. Anais do VII Simpósio Brasileiro de Sistemas de Informação, Páginas: 117 - 128. </p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-2"><p>Download</p></div>
+          <div class="col-md-10"><a href="#">link to ACM</a></div>
+        </div>
+      </section>
+
+
+
+
+
+      <section class="paper">
+        <div class="row">
+          <div class="col-md-12">
+            <h3>
+              <span class="glyphicon glyphicon-chevron-right"></span>
+              Ontology Refinement Through Theory Revision Techniques
             </h3>
           </div>
         </div>
@@ -262,9 +299,9 @@
           </div>
         </div>
         <div class="row">
-          <div class="col-md-2"><p>Reference</p></div>
+          <div class="col-md-2"><p>Citation</p></div>
           <div class="col-md-10">
-            <p>Leão, F., Revoredo, K., Baião, F. "Nome do Artigo", In Proceedings of whatever. 2011. (<a href="#">bibitex</a>)</p>
+            <p class="small">LEÃO, F. ; REVOREDO, K. ; BAIAO, F. “Ontology Refinement Through Theory Revision Techniques”. Proccedings of the III Workshop on Computational Intelligence, 2010, São Bernardo do Campo, SP. Páginas: 482 - 487. </p>
           </div>
         </div>
         <div class="row">
@@ -272,6 +309,10 @@
           <div class="col-md-10"><a href="#">link to ACM</a></div>
         </div>
       </section>
+
+
+
+
 
     <div id="push"></div>
 
@@ -284,33 +325,146 @@
           <div class="col-md-12">
             <h3>
               <span class="glyphicon glyphicon-chevron-right"></span>
-              ORION–Um Framework para Refinamento de Ontologias Através de Técnicas de Revisão de Teorias
+              Propagação de Identidade com o Uso do Framework Spring
             </h3>
           </div>
         </div>
         <div class="row">
-          <div class="col-md-12 authors"><p>Felipe Leão, Kate Revoredo and Fernanda Baião</p></div>
+          <div class="col-md-12 authors"><p>Felipe Leão, Leonardo Azevedo, Claudia Capelli and Fernanda Baião</p></div>
         </div>
         <div class="row">
           <div class="col-md-2"><p>Abstract</p></div>
           <div class="col-md-10">
-            Ontology have been frequently used for knowledge representation. As it evolves, however,
-            keeping its consistency becomes a costly and complex task, if no support is provided. This work proposes ORION
-            – Ontology Refinement through theory revisION, which applies existing Theory Revision techniques to support
-            Ontology evolution. Preliminary tests were successful in obtaining a more accurate refined Ontology.
+            Information security is a critical issue for organizations and comprises several perspectives.
+            Identity propagation and data access control are some of those important issues in current n-tiers
+            information systems architectures. This work describes the design and implementation of an architecture
+            for effectively assuring information security, based on standard technologies allied to modifications
+            to standard objects of the Spring framework, intending to propagate the user identity, in a non-intrusive
+            way, from client applications until the DBMS where the authorization rules are enforced using a RBAC
+            approach. Experimental testes were executed to evaluate the proposal concerning concurrent execution
+            and performance.
           </div>
         </div>
         <div class="row">
-          <div class="col-md-2"><p>Reference</p></div>
+          <div class="col-md-2"><p>Citation</p></div>
           <div class="col-md-10">
-            <p>Leão, F., Revoredo, K., Baião, F. "Nome do Artigo", In Proceedings of whatever. 2011. (<a href="#">bibitex</a>)</p>
+            <p class="small">LEAO, F., AZEVEDO, L. G., CAPPELI. C., BAIÃO, F.  “Propagação de Identidade com o Uso do Framework Spring”. Relatórios Técnicos do DIA/UNIRIO (RelaTe-DIA), RT-0003/2012, 2012 </p>
           </div>
         </div>
         <div class="row">
           <div class="col-md-2"><p>Download</p></div>
-          <div class="col-md-10"><a href="#">link to ACM</a></div>
+          <div class="col-md-10"><a href="http://www.seer.unirio.br/index.php/monografiasppgi/article/view/2152">report</a></div>
         </div>
       </section>
+
+
+
+      <section class="paper">
+          <div class="row">
+            <div class="col-md-12">
+              <h3>
+                <span class="glyphicon glyphicon-chevron-right"></span>
+                Propagação de Identidade em Acesso a Dados Via Serviços Web
+              </h3>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12 authors"><p>Felipe Leão, Leonardo Azevedo, Talles Santana, Fernanda Baião and Claudia Capelli</p></div>
+          </div>
+          <div class="row">
+            <div class="col-md-2"><p>Abstract</p></div>
+            <div class="col-md-10">
+              Information Security is one of the main issues in current organizations. In this context,
+              role-based data access control and user identity propagation in n-tier architectures are
+              foremost concerns. This work proposes a pragmatic architecture and its implementation that
+              handles these concerns on web services systems. The implementation is based on the SOAP
+              protocol and interceptor handlers. Experimental tests were conducted and positive results
+              were obtained regarding the proposal effectiveness, efficiency and robustness in concurrent scenarios.
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-2"><p>Citation</p></div>
+            <div class="col-md-10">
+              <p class="small">LEÃO, F., AZEVEDO, L., SANTANA, T., BAIÃO, F., CAPELLI, C., “Propagação de Identidade em Acesso a Dados Via Serviços Web”. Relatórios Técnicos do DIA/UNIRIO (RelaTe-DIA),              RT-0002/2012, 2012. Acessado em: Agosto de 2013.  </p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-2"><p>Download</p></div>
+            <div class="col-md-10"><a href="http://www.seer.unirio.br/index.php/monografiasppgi/article/view/2151">report</a></div>
+          </div>
+        </section>
+
+
+
+        <section class="paper">
+            <div class="row">
+              <div class="col-md-12">
+                <h3>
+                  <span class="glyphicon glyphicon-chevron-right"></span>
+                  BPEL: Principais Conceitos e Uso Prático
+                </h3>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-12 authors"><p>Arthur Moreira, Felipe Leão, Sandro Lopes, Flávio Faria and Leonardo Azevedo</p></div>
+            </div>
+            <div class="row">
+              <div class="col-md-2"><p>Abstract</p></div>
+              <div class="col-md-10">
+                This work presents the BPEL (Business Process Execution Language) pattern for service orchestration. It focus on
+                presenting the descriptions of essential constructs for its use. The goal of this work is to pose as a tutorial
+                taht facilitates the use of BPEL pattern for developers that doesn't have much experiencein working with web services.
+                Besides it is presented a literature review of recent works in orchestration.
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-2"><p>Citation</p></div>
+              <div class="col-md-10">
+                <p class="small">MOREIRA, A., LEÃO, F., LOPES, S., FARIA, F., AZEVEDO,L., BPEL: Principais Conceitos e Uso Prático”. Relatórios Técnicos do DIA/UNIRIO (RelaTe-DIA), RT-0011/2011, 2011. </p>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-2"><p>Download</p></div>
+              <div class="col-md-10"><a href="http://www.seer.unirio.br/index.php/monografiasppgi/article/viewFile/1591/1383">report</a></div>
+            </div>
+          </section>
+
+
+
+
+
+          <section class="paper">
+              <div class="row">
+                <div class="col-md-12">
+                  <h3>
+                    <span class="glyphicon glyphicon-chevron-right"></span>
+                    Propagação de Identidade em Aplicações em Três Camadas
+                  </h3>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12 authors"><p>Felipe Leão, Leonardo Azevedo, Flávio Faria, Fernanda Baião and Claudia Capelli</p></div>
+              </div>
+              <div class="row">
+                <div class="col-md-2"><p>Abstract</p></div>
+                <div class="col-md-10">
+                  Information security is a critical issue for organizations and comprises several perspectives, including
+                  that of information systems and data repositories development. In current distributed information systems
+                  architectures, information security involves identity propagation and data access authorization issues.
+                  This work describes the design and implementation of a simple, yet not trivial, architecture for effectively
+                  assuring information security, based on a series of standard technology.
+              </div>
+              <div class="row">
+                <div class="col-md-2"><p>Citation</p></div>
+                <div class="col-md-10">
+                  <p class="small">LEÃO, F., AZEVEDO, L., FARIA, F., BAIÃO, F., CAPPELLI, C. "Propagação de Identidade em Aplicações em Três Camadas". Relatórios Técnicos do DIA/UNIRIO (RelaTe-DIA), RT-0007/2012, 2011. </p>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-2"><p>Download</p></div>
+                <div class="col-md-10"><a href="http://www.seer.unirio.br/index.php/monografiasppgi/article/viewFile/1492/1377">report</a></div>
+              </div>
+            </section>
 
 
     <hr class="dashed" />
